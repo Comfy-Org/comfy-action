32f4282b05076b9e407d7b461c3f0be30f174178
Yoland Y <4950057+yoland68@users.noreply.github.com>
2024-04-04 19:04:56 -0700

    Update windows actions

--- start-server.ps1
+++ start-server.ps1
@@ -1,9 +1,11 @@
 # start-server.ps1
+param (
+    [String]$GITHUB_WORKSPACE
+)
 
 # Initialize Conda environment
 # TODO: remove hardcoding
-$Env:Path = "C:\Users\yoland68\miniconda3\shell\condabin;" + $Env:Path
 & conda activate comfyui
 
 # Start the web server and redirect output to a log file
-python "$Env:GITHUB_WORKSPACE/main.py" --force-fp16 *> "application.log"
+& python "$GITHUB_WORKSPACE/main.py" --force-fp16 *> "$GITHUB_WORKSPACE/application.log"
