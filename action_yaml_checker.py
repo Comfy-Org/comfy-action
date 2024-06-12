16f2f6c89d4cf9d4fabc0ac71afba6238576e5ae
Yoland Y <4950057+yoland68@users.noreply.github.com>
2024-06-12 14:25:52 -0700

    Feat. Refactor action yml file

---
+++ action_yaml_checker.py
@@ -0,0 +1,86 @@
+"""
+This python script helps check if action yaml files have step names and the
+windows actions are ligning up with the unix actions.
+
+e.g. > python action_yaml_checker.py action.yml
+
+"""
+import yaml
+from tabulate import tabulate
+import argparse
+
+parser = argparse.ArgumentParser(description="Check if action yaml files have step names and if windows actions are aligned with unix actions.")
+parser.add_argument("-c", "--complete", action="store_true", help="Include complete step information")
+parser.add_argument("yaml_file", help="Path to the YAML file")
+
+def conditional_zip(x, y, step_name_runs_map, complete: bool = False):
+    combined_list = []
+    i = 0  # Pointer for x
+    j = 0  # Pointer for y
+    while i < len(x) or j < len(y):
+
+        x_curr = x[i] if i < len(x) else ''
+        y_curr = y[j] if j < len(y) else ''
+
+        x_runs = step_name_runs_map[x_curr] if x_curr != '' else ''
+        y_runs = step_name_runs_map[y_curr] if y_curr != '' else ''
+
+        if '-Only]' in x_curr:
+            if complete:
+                x_curr = x_curr + '\n' + x_runs
+            combined_list.append([x_curr, ''])
+            i += 1
+            continue
+        if '-Only]' in y_curr:
+            if complete:
+                y_curr = y_curr + '\n' + y_runs
+            combined_list.append(['', y_curr])
+            j += 1
+            continue
+        if complete:
+            x_curr = x_curr + '\n' + x_runs
+            y_curr = y_curr + '\n' + y_runs
+        combined_list.append([x_curr, y_curr])
+        i += 1
+        j += 1
+
+    return combined_list
+
+def get_step_names(yaml_file, complete: bool = False):
+    with open(yaml_file, 'r') as file:
+        data = yaml.safe_load(file)
+    
+    steps = data['runs']['steps']
+
+    for step in steps:
+        if 'name' not in step:
+            raise Exception("Step name not found in the action file: {}".format(step))
+    
+    step_name_runs_map = {}
+        
+    step_names = [step['name'] for step in steps if '[Universal]' not in step['name']]
+    unix_step_names = []
+    wins_step_names = []
+    for step in steps:
+        if '[Unix' in step['name']:
+            unix_step_names.append(step['name'])
+
+        if '[Win' in step['name']:
+            wins_step_names.append(step['name'])
+        step_name_runs_map[step['name']] = step['run'] if 'run' in step else step.get('uses', '[No run or uses info]')
+
+    if len(unix_step_names) + len(wins_step_names) != len(step_names):
+        uncategorized_steps = set(step_names) - set(unix_step_names + wins_step_names)
+        raise Exception("There steps are not categorized: {}".format(uncategorized_steps))
+    
+    tabulated_list = conditional_zip(unix_step_names, wins_step_names, step_name_runs_map, complete=complete)
+    
+    print(tabulate(tabulated_list, headers=['Unix', 'Windows'], tablefmt='grid'))
+
+if __name__ == "__main__":
+
+        args = parser.parse_args()
+
+        yaml_file = args.yaml_file
+        get_step_names(yaml_file, args.complete)
+    
