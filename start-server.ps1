eb4513891f5aaab4281da9afc3e81e0c9f8b0e72
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-07-09 16:52:26 -0700

    minor fix

--- start-server.ps1
+++ start-server.ps1
@@ -5,11 +5,11 @@ param (
 
 # Initialize Conda environment
 # TODO: remove hardcoding
-echo Starting Conda
+echo "Starting Conda"
 conda activate comfyui
 
 # Start the web server and redirect output to a log file
-echo Running Server
+echo "Running Server"
 python "$GITHUB_WORKSPACE/main.py" --force-fp16 *> "$GITHUB_WORKSPACE/application.log"
 
-echo Done
+echo "Done"
