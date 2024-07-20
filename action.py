9c726f4f508d83fa0ada1284c8d7474e27b4b1cd
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-07-19 18:18:40 -0700

    add python version and cuda version to outfile path

--- action.py
+++ action.py
@@ -151,7 +151,7 @@ def main(args):
     counter = 1
 
     for workflow_file_name in workflow_files:
-        gs_path = make_unix_safe(f"output-files/{args.github_action_workflow_name}-{args.os}-{workflow_file_name}-run-{args.run_id}")
+        gs_path = make_unix_safe(f"output-files/{args.github_action_workflow_name}-{args.os}-{args.python_version}-{args.cuda_version}-{workflow_file_name}-run-{args.run_id}")
         send_payload_to_api(args, gs_path, workflow_file_name, 0, 0, WfRunStatus.Started)
         # Construct the file path
         file_path = f"workflows/{workflow_file_name}"
